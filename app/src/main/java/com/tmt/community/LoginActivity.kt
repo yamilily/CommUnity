a044bb270baf210f0c29411d9b76e69ded7ad50b
YurikaFITB <yurika.svsky+github@gmail.com>
2025-11-12 03:27:13 +0800

    commit

--- app/src/main/java/com/tmt/community/LoginActivity.kt
+++ app/src/main/java/com/tmt/community/LoginActivity.kt
@@ -1,14 +1,48 @@
 package com.tmt.community
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.airbnb.lottie.LottieAnimationView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // This is the only line we need for now. It connects this code
-        // file to our beautiful XML layout.
+        val splashScreen = installSplashScreen()
+
         setContentView(R.layout.activity_login)
+        splashScreen.setOnExitAnimationListener { splashScreenView ->
+            val lottieView = LottieAnimationView(baseContext).apply {
+                setAnimation(R.raw.app_logo_animation)
+                scaleType = ImageView.ScaleType.FIT_CENTER
+                repeatCount = 0
+                playAnimation()
+            }
+
+            val splashScreenLayout = splashScreenView.view as ViewGroup
+
+            splashScreenLayout.addView(lottieView,
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT)
+
+            val alphaOut = ObjectAnimator.ofFloat(
+                splashScreenView.view,
+                View.ALPHA,
+                1f,
+                0f
+            )
+            alphaOut.duration = 400L
+            alphaOut.doOnEnd {
+                splashScreenView.remove()
+            }
+
+            alphaOut.start()
+        }
     }
 }
