1e7ed92a466bb8ac9fa3016127b2b5509d8a5923
YurikaFITB <yurika.svsky+github@gmail.com>
2025-11-12 03:34:59 +0800

    commit

--- app/src/main/java/com/tmt/community/LoginActivity.kt
+++ app/src/main/java/com/tmt/community/LoginActivity.kt
@@ -1,48 +1,32 @@
 package com.tmt.community
 
-import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
-import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.airbnb.lottie.LottieAnimationView
 
 class LoginActivity : AppCompatActivity() {
+
+    private var isAppReady = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val splashScreen = installSplashScreen()
-
         setContentView(R.layout.activity_login)
-        splashScreen.setOnExitAnimationListener { splashScreenView ->
-            val lottieView = LottieAnimationView(baseContext).apply {
-                setAnimation(R.raw.app_logo_animation)
-                scaleType = ImageView.ScaleType.FIT_CENTER
-                repeatCount = 0
-                playAnimation()
+        val content: View = findViewById(android.R.id.content)
+        content.viewTreeObserver.addOnPreDrawListener {
+            if (isAppReady) {
+                true
+            } else {
+                false
             }
-
-            val splashScreenLayout = splashScreenView.view as ViewGroup
-
-            splashScreenLayout.addView(lottieView,
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT)
-
-            val alphaOut = ObjectAnimator.ofFloat(
-                splashScreenView.view,
-                View.ALPHA,
-                1f,
-                0f
-            )
-            alphaOut.duration = 400L
-            alphaOut.doOnEnd {
-                splashScreenView.remove()
-            }
-
-            alphaOut.start()
         }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            isAppReady = true
+        }, 2000L)
     }
 }
