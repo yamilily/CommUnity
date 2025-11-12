d45c4f16852ac59f68f6520deca5ff6d6c12e314
YurikaFITB <yurika.svsky+github@gmail.com>
2025-11-13 02:39:20 +0800

    commit 02:25AM - 11/13/2025

---
+++ app/src/main/java/com/tmt/community/ui/menu/MenuFragment.kt
@@ -0,0 +1,38 @@
+package com.tmt.community.ui.menu // The package name might still be this, which is fine
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.tmt.community.LoginActivity
+import com.tmt.community.R
+
+class MenuFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_menu, container, false)
+
+        val logoutButton: Button = view.findViewById(R.id.logout_button)
+
+        logoutButton.setOnClickListener {
+            // Sign out the user from Firebase
+            Firebase.auth.signOut()
+
+            // Go back to the Login Screen and clear all previous screens
+            val intent = Intent(activity, LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
+
+        return view
+    }
+}
--- app/src/main/java/com/tmt/community/ui/menu/MenuViewModel.kt
+++ app/src/main/java/com/tmt/community/ui/menu/MenuViewModel.kt
@@ -1,13 +1,13 @@
-package com.tmt.community.ui.dashboard
+package com.tmt.community.ui.menu
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class MenuViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is menu Fragment"
     }
     val text: LiveData<String> = _text
 }
